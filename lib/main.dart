c79c782872af056b3d4ccb712b73cdfa87c7b078
PedroHFerreira <pedrohsxd@gmail.com>
2022-01-27 19:13:11 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Transferencias')),
+        floatingActionButton:
+            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+      ),
+    ));
