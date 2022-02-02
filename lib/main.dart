db36662442f6587b2fad2f9722836237d5482843
PedroHFerreira <pedrohsxd@gmail.com>
2022-02-02 17:08:46 -0300

    extraction

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
       home: Scaffold(
+        body: ListaTransferencia(),
         appBar: AppBar(title: Text('Transferencias')),
         floatingActionButton:
             FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
       ),
     ));
+
+class ListaTransferencia extends StatelessWidget {
+  const ListaTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      // ignore: prefer_const_literals_to_create_immutables
+      children: [
+        ItemTransferencia(Transferencia(numeroConta: 1000, valor: 100)),
+        ItemTransferencia(Transferencia(numeroConta: 1001, valor: 500)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia({
+    required this.valor,
+    required this.numeroConta,
+  });
+}
