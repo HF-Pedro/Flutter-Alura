6ba859dd6ba212e08c7c4a023592842d4cd4e7a8
PedroHFerreira <pedrohsxd@gmail.com>
2022-02-08 03:43:44 -0300

    interfaces

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,69 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: ListaTransferencia(),
-        appBar: AppBar(title: Text('Transferencias')),
-        floatingActionButton:
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(title: Text("Registro de Transferência")),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 24),
+                decoration: InputDecoration(
+                    labelText: "Número da Conta", hintText: "0000"),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                style: TextStyle(fontSize: 24),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: "Valor",
+                    hintText: "0.00"),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("Confirmar")),
+          ],
+        ));
+  }
+}
 
 class ListaTransferencia extends StatelessWidget {
   const ListaTransferencia({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // ignore: prefer_const_literals_to_create_immutables
-      children: [
-        ItemTransferencia(Transferencia(numeroConta: 1000, valor: 100)),
-        ItemTransferencia(Transferencia(numeroConta: 1001, valor: 500)),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: Text('Transferencias')),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          ItemTransferencia(Transferencia(numeroConta: 1000, valor: 100)),
+          ItemTransferencia(Transferencia(numeroConta: 1001, valor: 500)),
+        ],
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
     );
   }
 }
